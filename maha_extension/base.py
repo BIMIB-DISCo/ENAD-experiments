33d68f274d0b2d9bae341ef839ca663ca5ac2813
thegraystreet <craighero.francesco+github@gmail.com>
2022-11-24 18:06:29 +0000

    major release

--- maha_extension/base.py
+++
@@ -1,6 +0,0 @@
-ALL_NET_TYPES = ['resnet', 'densenet']
-ALL_DATASETS = ['cifar10', 'svhn', 'cifar100']
-ALL_ADV_TYPES = ['DeepFool', 'FGSM', 'BIM', 'CWL2']
-
-n_classes = lambda dataset: 100 if dataset == 'cifar100' else 10
-n_layers = lambda net_type: 5 if net_type == 'resnet' else 4
--- utils.py
+++
@@ -1,7 +0,0 @@
-from sklearn.metrics import precision_recall_curve, auc
-
-def aupr(y_true, y_pred, pos_label=1):
-
-    precision, recall, _ = precision_recall_curve(y_true, y_pred, pos_label=pos_label)
-
-    return auc(recall, precision)
